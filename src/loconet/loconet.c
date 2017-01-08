4d8c1f2c3b083156ac188b112eb39839aee6d03a
Ferdi van der Werf <efcm@vdwerf.eu>
2017-01-08 13:22:24 +0100

    Add base for loconet

---
+++ src/loconet/loconet.c
@@ -0,0 +1,10 @@
+/**
+ * @file loconet.c
+ * @brief Loconet base functionality
+ *
+ * \copyright Copyright 2017 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <ferdi@slashdev.nl>
+ */
+#include "loconet.h"
---
+++ src/loconet/loconet.h
@@ -0,0 +1,16 @@
+/**
+ * @file loconet.h
+ * @brief Loconet base functionality
+ *
+ * \copyright Copyright 2017 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ *  Before you can use the logger functions, initialize the logger
+ *  using `logger_init(baudrate);`.
+ *
+ * @author Ferdi van der Werf <ferdi@slashdev.nl>
+ */
+
+#ifndef _LOCONET_LOCONET_H_
+#define _LOCONET_LOCONET_H_
+#endif // _LOCONET_LOCONET_H_
