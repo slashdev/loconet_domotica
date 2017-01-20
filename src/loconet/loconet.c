d42b6e256e10666d3d52076ae15e7816b63fd41a
Ferdi van der Werf <efcm@vdwerf.eu>
2017-01-20 22:30:15 +0100

    Only free a message if there is one

--- src/loconet/loconet.c
+++ src/loconet/loconet.c
@@ -707,8 +707,11 @@ void ln_handler_dummy_n(uint8_t *d, uint8_t l)
 void loconet_tx_stop(void)
 {
   loconet_status.bit.TRANSMIT = 0;
-  free(loconet_tx_current->data);
-  free(loconet_tx_current);
+  // We might not have a message due to collision detection
+  if (loconet_tx_current) {
+    free(loconet_tx_current->data);
+    free(loconet_tx_current);
+  }
 }
 
 //-----------------------------------------------------------------------------
