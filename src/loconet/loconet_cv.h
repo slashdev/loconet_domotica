d188678eb558074cc1424730636b356044a3a1b0
Ferdi van der Werf <efcm@vdwerf.eu>
2017-01-29 12:30:45 +0100

    Typedef the layout of a prog message

--- src/loconet/loconet_cv.h
+++ src/loconet/loconet_cv.h
@@ -11,4 +11,18 @@
 
 #ifndef _LOCONET_LOCONET_CV_H_
 #define _LOCONET_LOCONET_CV_H_
+
+#include <stdint.h>
+
+typedef struct {
+  uint8_t source;
+  __attribute__((packed)) uint16_t destination;
+  uint8_t request_id;
+  uint8_t most_significant_bits;
+  __attribute__((packed)) uint16_t device_class;
+  __attribute__((packed)) uint16_t lncv_number;
+  __attribute__((packed)) uint16_t lncv_value;
+  uint8_t flags;
+} LOCONET_CV_MSG_Type;
+
 #endif // _LOCONET_LOCONET_CV_H_
