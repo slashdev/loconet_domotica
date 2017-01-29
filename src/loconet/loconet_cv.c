e449d1697dad018fe01ede730a77630ce55fa731
Ferdi van der Werf <efcm@vdwerf.eu>
2017-01-29 12:23:46 +0100

    Add files for CV processing

---
+++ src/loconet/loconet_cv.c
@@ -0,0 +1,12 @@
+/**
+ * @file loconet_cv.c
+ * @brief Process Loconet CV messages
+ *
+ * \copyright Copyright 2017 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <ferdi@slashdev.nl>
+ * @author Jan Martijn van der Werf <janmartijn@slashdev.nl>
+ */
+
+#include "loconet_cv.h"
---
+++ src/loconet/loconet_cv.h
@@ -0,0 +1,14 @@
+/**
+ * @file loconet_cv.h
+ * @brief Process Loconet CV messages
+ *
+ * \copyright Copyright 2017 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <ferdi@slashdev.nl>
+ * @author Jan Martijn van der Werf <janmartijn@slashdev.nl>
+ */
+
+#ifndef _LOCONET_LOCONET_CV_H_
+#define _LOCONET_LOCONET_CV_H_
+#endif // _LOCONET_LOCONET_CV_H_
