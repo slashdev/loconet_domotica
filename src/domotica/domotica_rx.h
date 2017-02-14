d519ef2431e393825b9ef06a8d995e9eeb5f4277
Jan Martijn van der Werf <jm.vanderwerf@znck.nl>
2017-02-14 22:31:30 +0100

    Initial version that listens to b2 messages (opc_input_rep) It has a list to store the addresses with the corresponding lncv value.

---
+++ src/domotica/domotica_rx.h
@@ -0,0 +1,38 @@
+/*
+ * @file domotica_rx.h
+ * @brief Loconet Domotica Module - receiving messages
+ *
+ * \copyright Copyright 2017 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Jan Martijn van der Werf
+ */
+
+// ------------------------------------------------------------------
+
+#ifndef _DOMOTICA_RX_H_
+#define _DOMOTICA_RX_H_
+
+#define DOMOTICA_RX_INPUT_ADDRESS_SIZE 6
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "domotica.h"
+#include "loconet/loconet_cv.h"
+#include "loconet/loconet_rx.h"
+
+
+// ------------------------------------------------------------------
+// Add a B2 address to listen to.
+extern void domotica_rx_set_input_address(uint8_t lncv, uint16_t address);
+
+// ------------------------------------------------------------------
+// Remove a B2 address to listen to.
+void domotica_rx_remove_input_address(uint8_t lncv);
+
+// ------------------------------------------------------------------
+// Listen to sensor messages sent by other devices
+extern void loconet_rx_input_rep(uint8_t, uint8_t);
+
+#endif // _DOMOTICA_RX_H_
