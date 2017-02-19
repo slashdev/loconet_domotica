95e85dcb4c4ea628976f665421ecdc8c1c4e4612
Jan Martijn van der Werf <jm.vanderwerf@znck.nl>
2017-02-19 15:58:23 +0100

    Added macros to decide the function of an lncv number in the INPUT ADDRESS and FAST CLOCK ranges

--- src/domotica/domotica_cv.h
+++ src/domotica/domotica_cv.h
@@ -45,6 +45,20 @@
 #ifndef _DOMOTICA_CV_H_
 #define _DOMOTICA_CV_H_
 
+// ----------------------------------------------------------------------------
+// The different LNCV numbers used in the modulo calculation to decide
+// what function the LNCV number has in the input address range.
+#define DOMOTICA_LNCV_INPUT_ADDRESS_POS_ADDRESS (DOMOTICA_LNCV_INPUT_ADDRESSES_START % 5)
+#define DOMOTICA_LNCV_INPUT_ADDRESS_POS_MODH_ON ((DOMOTICA_LNCV_INPUT_ADDRESSES_START+1) % 5)
+#define DOMOTICA_LNCV_INPUT_ADDRESS_POS_MODH_OFF ((DOMOTICA_LNCV_INPUT_ADDRESSES_START+2) % 5)
+#define DOMOTICA_LNCV_INPUT_ADDRESS_POS_MODL_ON ((DOMOTICA_LNCV_INPUT_ADDRESSES_START+3) % 5)
+#define DOMOTICA_LNCV_INPUT_ADDRESS_POS_MODL_OFF ((DOMOTICA_LNCV_INPUT_ADDRESSES_START+4) % 5)
+
+// The different LNCV numbers used in the modulo calculation to decide
+// what function the LNCV number has in the FAST CLOCK range.
+#define DOMOTICA_LNCV_FAST_CLOCK_POS_TIME (DOMOTICA_LNCV_FAST_CLOCK_START % 3)
+#define DOMOTICA_LNCV_FAST_CLOCK_POS_MASK_ON ((DOMOTICA_LNCV_FAST_CLOCK_START + 1) % 3)
+#define DOMOTICA_LNCV_FAST_CLOCK_POS_MASK_OFF ((DOMOTICA_LNCV_FAST_CLOCK_START + 2) % 3)
 
 // ----------------------------------------------------------------------------
 void loconet_cv_written_event(uint16_t lncv_number, uint16_t value);
