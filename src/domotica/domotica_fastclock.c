e10e01950efb2d3c05ddcd341e19770c15eef50f
Jan Martijn van der Werf <jm.vanderwerf@znck.nl>
2017-02-19 16:19:57 +0100

    Skeleton for reacting to the fast clock

---
+++ src/domotica/domotica_fastclock.c
@@ -0,0 +1,9 @@
+/*
+ * @file domotica_fastclock.c
+ * @brief Loconet Domotica Module - listen to the fast clock
+ *
+ * \copyright Copyright 2017 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Jan Martijn van der Werf
+ */
---
+++ src/domotica/domotica_fastclock.h
@@ -0,0 +1,14 @@
+/*
+ * @file domotica_fastclock.h
+ * @brief Loconet Domotica Module - listen to the fast clock
+ *
+ * \copyright Copyright 2017 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Jan Martijn van der Werf
+ */
+
+#ifndef _DOMOTICA_FASTCLOCK_H_
+#define _DOMOTICA_FASTCLOCK_H_
+
+#endif
