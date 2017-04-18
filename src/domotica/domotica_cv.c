3df51f90c68a6713e76468e2e72454a403089834
Jan Martijn van der Werf <jm.vanderwerf@znck.nl>
2017-04-18 22:01:47 +0200

    Simplified the if statements for writing the CV values, as half of the statements were already implied by the "else" part of the statements.

--- src/domotica/domotica_cv.c
+++ src/domotica/domotica_cv.c
@@ -13,16 +13,17 @@
 // ----------------------------------------------------------------------------
 void loconet_cv_written_event(uint16_t lncv_number, uint16_t value)
 {
-  if (lncv_number == 3)
-  {
-    // Fast clock setting
+  if (lncv_number < DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_START) {
+    // do nothing. No values are set her
   }
-  else if (lncv_number >= DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_START && lncv_number < DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_END)
+  // By def, we have lncv_number >= DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_START
+  else if (lncv_number < DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_END)
   {
     // Output light settings
     domotica_set_output_brightness(lncv_number - DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_START, (uint8_t) value);
   }
-  else if (lncv_number >= DOMOTICA_LNCV_INPUT_ADDRESSES_START && lncv_number < DOMOTICA_LNCV_INPUT_ADDRESSES_END)
+  // By def, we have lncv_number >= DOMOTICA_LNCV_INPUT_ADDRESSES_START
+  else if (lncv_number < DOMOTICA_LNCV_INPUT_ADDRESSES_END)
   {
     if (lncv_number % 5 == DOMOTICA_LNCV_INPUT_ADDRESS_POS_ADDRESS)
     {
@@ -30,7 +31,8 @@ void loconet_cv_written_event(uint16_t lncv_number, uint16_t value)
       domotica_rx_set_input_address(lncv_number, value);
     }
   }
-  else if (lncv_number >= DOMOTICA_LNCV_FASTCLOCK_START && lncv_number < DOMOTICA_LNCV_FASTCLOCK_END)
+  // By def, we have lncv_number >= DOMOTICA_LNCV_FASTCLOCK_START
+  else if (lncv_number < DOMOTICA_LNCV_FASTCLOCK_END)
   {
     if (lncv_number % 3 == DOMOTICA_LNCV_FASTCLOCK_POS_TIME)
     {
@@ -43,29 +45,22 @@ void loconet_cv_written_event(uint16_t lncv_number, uint16_t value)
 // ----------------------------------------------------------------------------
 uint8_t loconet_cv_write_allowed(uint16_t lncv_number, uint16_t value)
 {
-  if (lncv_number == 0)
-  {
-    return (value > 0) ? LOCONET_CV_ACK_OK : LOCONET_CV_ACK_ERROR_OUTOFRANGE;
+  if ( lncv_number > 3 && lncv_number < DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_START) {
+    return LOCONET_CV_ACK_ERROR_INVALID_VALUE;
   }
-  else if (lncv_number == 2)
-  {
-    return (value > 0 && value <= 0x0F) ? LOCONET_CV_ACK_OK : LOCONET_CV_ACK_ERROR_OUTOFRANGE;
-  }
-  else if (lncv_number == 3)
-  {
-    // Fast clock setting
-    return (value < 3) ? LOCONET_CV_ACK_OK : LOCONET_CV_ACK_ERROR_OUTOFRANGE;
-  }
-  else if (lncv_number >= DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_START && lncv_number < DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_END)
+  // By def we have lncv_number >= DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_START
+  else if (lncv_number < DOMOTICA_LNCV_OUTPUT_BRIGHTNESS_END)
   {
     // Output light settings
     return (value <= DOMOTICA_OUTPUT_MAX_BRIGHTNESS) ? LOCONET_CV_ACK_OK : LOCONET_CV_ACK_ERROR_OUTOFRANGE;
   }
-  else if (lncv_number >= DOMOTICA_LNCV_INPUT_ADDRESSES_START && lncv_number < DOMOTICA_LNCV_INPUT_ADDRESSES_END)
+  // By def, we have lncv_number >= DOMOTICA_LNCV_INPUT_ADDRESSES_START
+  else if (lncv_number < DOMOTICA_LNCV_INPUT_ADDRESSES_END)
   {
     return LOCONET_CV_ACK_OK;
   }
-  else if (lncv_number >= DOMOTICA_LNCV_FASTCLOCK_START && lncv_number < DOMOTICA_LNCV_FASTCLOCK_END)
+  // By def, we have lncv_number >= DOMOTICA_LNCV_FASTCLOCK_START
+  else if (lncv_number < DOMOTICA_LNCV_FASTCLOCK_END)
   {
     return LOCONET_CV_ACK_OK;
   }
