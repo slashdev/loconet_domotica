4f7f4026a1bd08e928a8ca9d5a86a9badf88ad68
Jan Martijn van der Werf <jm.vanderwerf@znck.nl>
2017-04-19 22:33:37 +0200

    Added comments on how to use the stuff.

--- src/outputhandler/outputhandler.h
+++ src/outputhandler/outputhandler.h
@@ -6,6 +6,21 @@
  * \license This project is released under MIT license.
  *
  * @author Jan Martijn van der Werf
+ *
+ * Dummy functions that can be implemented to actually control the output
+ * changes:
+ *  - outputhandler_switch_state_pre_event(uint16_t state)
+ *  - outputhandler_switch_state_post_event(uint16_t state)
+ *  - outputhandler_set_output_state(uint8_t output, uint8_t brightness)
+ *
+ * The latter function is called for each output, with brightness varying
+ * between 0 and DOMOTICA_OUTPUT_MAX_BRIGHTNESS, where 0 is used to indicate
+ * that the output should be switched off.
+ *
+ * The first two functions are called upon changing the state, i.e., first
+ *  the event `output_switch_state_pre_event` is called, then for each output
+ *  that requires a change `outputhandler_set_output_state` is called, and
+ *  finally `output_switch_state_post_event` is called.
  */
 
 #include <stdint.h>
